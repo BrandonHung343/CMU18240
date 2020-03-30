f2251a4f3a51057564660cf48a850e3735c29acd
Brandon Hung <brandonhung343@gmail.com>
2020-03-29 23:00:08 -0700

    NANI DESUKA

---
+++ hw8/OnesCount_test.sv
@@ -0,0 +1,87 @@
+`default_nettype none
+module OnesCount_test();
+
+  logic d_in_ready, clock, reset, dor;
+  logic [29:0] d_in;
+  logic [5:0] d_out;
+  
+  initial begin
+    $monitor($time,, "d_in_ready=%b, dor = %b, d_out=%d", 
+                      d_in_ready, dor, d_out);
+    clock = 0;
+    forever #5 clock = ~clock;
+  end
+  
+  OnesCount ms (.*);
+
+  initial begin
+    d_in = 30'h3FFF
+    @(posedge clock);
+    @(posedge clock);
+    d_in_ready = 1;
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    d_in_ready = 0;
+    d_in = 30'h3FFFFFFF
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    #1 $finish;
+  end
+  
+endmodule: OnesCount_test
