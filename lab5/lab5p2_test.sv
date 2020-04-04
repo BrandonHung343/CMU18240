0dd6295207d19193e18faa439bfdc51193e9382a
Brandon Hung <brandonhung343@gmail.com>
2020-04-04 16:49:23 -0700

    Try

--- lab5/lab5p2_test.sv
+++ lab5/lab5p2_test.sv
@@ -10,17 +10,21 @@ module lab5p2_test ();
   lab5p2 psm(.*);
   initial begin
     $monitor($time,, "ready=%b, reset_N = %b, done=%b, error=%b\
-                      state=%s, found_it=%b, dna_length=%d\
-                      nuc=%b, pat=%h, wordCount=%d, wordGt=%b\
-                      fsm_notif=%b, nextState=%s, patCount=%d\
+                      state=%s, found_it=%b, dna_length=%d \n\
+                      nuc=%b, pat=%h, wordCount=%d, wordEq=%b\
+                      fsm_notif=%b, nextState=%s, patCount=%d\n\
                       en_wc=%b, en_pc=%b, cl_wc=%b, cl_pc=%b\
-                      re_p=%b, re_s=%b, ld_pc=%b, ld_wc=%b", 
+                      re_p=%b, re_s=%b, en_tmp=%b, ld_tmp=%b\n\
+                      muxedPatCount=%d, checkAhead=%d\
+                      patternSignal=%d, ld_wc=%b, ld_pc=%b", 
                       ready, reset_N, done, error, psm.FSM.state.name, 
                       found_it, dna_length, psm.nuc, psm.pattern, 
-                      psm.WordCount, psm.seqGt, psm.fsm_notif,
+                      psm.WordCount, psm.seqEq, psm.fsm_notif,
                       psm.FSM.nextState.name, psm.PatternCount,
                       psm.en_wc, psm.en_pc, psm.cl_wc, psm.cl_pc,
-                      psm.re_p, psm.re_s, psm.ld_pc, psm.ld_wc);
+                      psm.re_p, psm.re_s, psm.en_tmp, psm.ld_tmp,
+                      psm.MuxedPatCount, psm.checkPatAhead, 
+                      psm.patternSignal, psm.ld_wc, psm.ld_pc);
     clock = 0;
     forever #5 clock = ~clock;
   end
