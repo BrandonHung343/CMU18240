7ad8d324698a6b88b42bc9d88c7b39bbdc511bd0
brandonhung343 <brandonhung343@gmail.com>
2020-04-12 03:03:40 -0400

    Prob3 done

--- hw9/hw9prob3.asm
+++ hw9/hw9prob3.asm
@@ -1,11 +1,11 @@
     .ORG $1234
-A   .DW $a ; val of a
-B   .DW $b ; val of b
+A   .DW $e ; val of a
+B   .DW $e ; val of b
 RES .DW $0
 
     .ORG $1000
-     LW R2, A, $0 ; load value of A into R2
-     LW R3, B, $0 ; load value of B into R3
+     LW R2, R0, A ; load value of A into R2
+     LW R3, R0, B ; load value of B into R3
      SLTI R0, R2, $0 ; see if A is 0
      BRZ resa ; go to end, store A in RES if yes
      SLTI R0, R3, $0 ; see if B is 0
@@ -20,10 +20,11 @@ loop SLT R4, R2, R3 ; compare the registers
      ; a < b, so b = b - a
 altb SUB R3, R3, R2
      BRA loop ; back to loop
-resa SW R0, RES, R2 ; store M[RES] = a
+resa SW R0, R2, RES ; store M[RES] = a
      BRA done ; go to end
-resb SW R0, RES, R3 ; store M[RES] = b
-done STOP;
+resb SW R0, R3, RES ; store M[RES] = b
+done LW R7, R0, RES;
+     STOP
     
 
      
